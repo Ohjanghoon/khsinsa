98ba857a81a3ee408fa585541c4b413461dbb1ed
lno <lno@DESKTOP-OT475ST>
2022-07-18 17:54:00 +0900

    Community DTO 패키지에 클래스(enum포함) 5개 추가!

--- src/main/java/com/kh/sinsa/community/model/dto/Community.java
+++ src/main/java/com/kh/sinsa/community/model/dto/Community.java
@@ -1,5 +1,96 @@
 package com.kh.sinsa.community.model.dto;
 
-public class Community {
+import java.sql.Date;
 
+public class Community {
+	private String commNo;
+	private String userId;
+	private String commTitle;
+	private String commContent;
+	private Date commDate;
+	private int commRecommand;
+	private int commReadCount;
+	
+	public Community() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Community(String commNo, String userId, String commTitle, String commContent, Date commDate,
+			int commRecommand, int commReadCount) {
+		super();
+		this.commNo = commNo;
+		this.userId = userId;
+		this.commTitle = commTitle;
+		this.commContent = commContent;
+		this.commDate = commDate;
+		this.commRecommand = commRecommand;
+		this.commReadCount = commReadCount;
+	}
+
+	public String getCommNo() {
+		return commNo;
+	}
+
+	public void setCommNo(String commNo) {
+		this.commNo = commNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getCommTitle() {
+		return commTitle;
+	}
+
+	public void setCommTitle(String commTitle) {
+		this.commTitle = commTitle;
+	}
+
+	public String getCommContent() {
+		return commContent;
+	}
+
+	public void setCommContent(String commContent) {
+		this.commContent = commContent;
+	}
+
+	public Date getCommDate() {
+		return commDate;
+	}
+
+	public void setCommDate(Date commDate) {
+		this.commDate = commDate;
+	}
+
+	public int getCommRecommand() {
+		return commRecommand;
+	}
+
+	public void setCommRecommand(int commRecommand) {
+		this.commRecommand = commRecommand;
+	}
+
+	public int getCommReadCount() {
+		return commReadCount;
+	}
+
+	public void setCommReadCount(int commReadCount) {
+		this.commReadCount = commReadCount;
+	}
+
+	@Override
+	public String toString() {
+		return "Community [commNo=" + commNo + ", userId=" + userId + ", commTitle=" + commTitle + ", commContent="
+				+ commContent + ", commDate=" + commDate + ", commRecommand=" + commRecommand + ", commReadCount="
+				+ commReadCount + "]";
+	}
+	
+	
+	
 }

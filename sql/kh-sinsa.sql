95b9a486e7d8bd19644be7d8b491ccf316237a6d
ilchan <winters63@naver.com>
2022-07-21 22:04:02 +0900

    상품 장바구니 추가!!

--- sql/kh-sinsa.sql
+++ sql/kh-sinsa.sql
@@ -420,3 +420,4 @@ alter table kh_order modify order_address varchar2(2000);
 select * from favorite;
 select count(*) from favorite where pro_no = ?;
 insert into favorite values(?,?);
+select * from cart;
